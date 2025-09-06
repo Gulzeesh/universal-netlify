aa77250a1258dd1fc59602f26220e96bb846a5ec
Gulzeesh <gulzeesh@ctruh.com>
2025-09-07 01:21:56 +0530

    model update

--- src/app/explore/[slug]/components/PointerList.tsx
+++ src/app/explore/[slug]/components/PointerList.tsx
@@ -5,7 +5,7 @@ const PointerList = ({ title, pointers }: PointerListProps) => {
   return (
     <div
       className="bg-background-200 border-background-400 flex w-[424px] min-w-[424px] flex-grow flex-col justify-between rounded-xl border-2 p-8"
-      style={{ zoom: 0.72 }}
+      style={{ zoom: 0.62 }}
     >
       <h2 className="font-dm-sans text-[40px] leading-14 font-bold -tracking-[0.8px] text-neutral-500">
         {title}
